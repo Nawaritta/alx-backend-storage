d378555fe087519649c35a72b0e1577f4824b759
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-18 15:50:53 +0000

    tasks 9, 10, 11

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,11 @@
+-- divides (and returns) the first by the second number
+DELIMITER //
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN
+	RETURN CASE
+	    WHEN b = 0 THEN 0
+    	ELSE a / b
+    END CASE;
+END;
+DELIMITER
