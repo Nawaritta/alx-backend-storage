80976ce49cf92645a3834d34fc4841726fbd7eeb
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-18 16:01:32 +0000

    delimiter

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,11 +1,12 @@
 -- divides (and returns) the first by the second number
-DELIMITER //
+DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 BEGIN
-	RETURN CASE
-    	WHEN b = 0 THEN 0
+    RETURN CASE
+        WHEN b = 0 THEN 0
         ELSE a / b
-    END CASE;
+    END;
 END;
+$$
 DELIMITER ;
