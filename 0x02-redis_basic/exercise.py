37204f28dda46b6e0af2207babaa1121c49dc7f1
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-24 20:14:31 +0000

    tasks 2 to 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,53 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count how many times a method is called"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs
+    for a function in Redis"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapper function"""
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+
+        self._redis.rpush(input_key, str(args))
+
+        result = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(result))
+
+        return result
+
+    return wrapper
+
+
+def replay(self, method: Callable):
+    """display the history of calls of a particular function"""
+    input_key = method.__qualname__ + ":inputs"
+    output_key = method.__qualname__ + ":outputs"
+
+    inputs = self._redis.lrange(input_key, 0, -1)
+    outputs = self._redis.lrange(output_key, 0, -1)
+
+    for i, (input_args, output) in enumerate(zip(inputs,
+                                                 outputs), start=1):
+        print("{}(*{}) -> {}"
+              .format(i, input_args.decode('utf-8',
+                                           output.decode('utf-8'))))
 
 
 class Cache:
@@ -15,6 +62,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generate a random key using uuid
         Store the input data in Redis with the random key
@@ -24,7 +73,8 @@ class Cache:
         return rand_key
 
     def get(self, key: str,
-            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+            fn: Optional[Callable]
+            = None) -> Union[str, bytes, int, float]:
         """Retrieve the data from Redis using the key
         and if data is None, return None"""
         data = self._redis.get(key)
